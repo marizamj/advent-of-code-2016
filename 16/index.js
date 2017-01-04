9523a464258dfe7f496de8e6958af7b8b66b88e0
Marie <marizamj@gmail.com>
2017-01-04 20:34:26 +0100

    Day 16

--- 16/index.js
+++ 16/index.js
@@ -1,3 +1,36 @@
-const fs = require('fs');
+const diskLength_1 = 272;
+const diskLength_2 = 35651584;
+const initData = '11110010111001001';
 
-const input = fs.readFileSync('input.txt', 'utf8');
+const generateNewData = data => {
+  const replace = {
+    '0': 1,
+    '1': 0
+  };
+
+  const a = data;
+  const b = data.split('').reverse().map(el => replace[el]).join('');
+
+  return `${a}0${b}`;
+};
+
+const generateEnoughForDisk = (data, diskLength) =>
+  data.length >= diskLength ?
+    data.slice(0, diskLength)
+    :
+    generateEnoughForDisk(generateNewData(data), diskLength);
+
+const getChecksum = data => {
+  const checksum = data
+    .match(/\d{2}/g)
+    .map(pair => pair.charAt(0) === pair.charAt(1) ? 1 : 0).join('');
+
+  return checksum.length % 2 !== 0 ? checksum : getChecksum(checksum);
+};
+
+const processData = (data, diskLength) => {
+  const finalData = generateEnoughForDisk(data, diskLength);
+  return getChecksum(finalData);
+}
+
+console.log(processData(initData, diskLength_2));
