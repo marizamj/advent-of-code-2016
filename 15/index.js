afda7f41dc13aa750b2b6d410dd0599ceb6eb4e5
Marie <marizamj@gmail.com>
2017-01-04 19:51:26 +0100

    Day 15

--- 15/index.js
+++ 15/index.js
@@ -6,3 +6,44 @@ Disc #4 has 13 positions; at time=0, it is at position 7.
 Disc #5 has 5 positions; at time=0, it is at position 1.
 Disc #6 has 3 positions; at time=0, it is at position 0.
 `;
+
+const parseInput = input =>
+  input.trim().split('\n').map(line => {
+    const number = Number(line.match(/\#(\d+)/)[1]);
+    const positions = Number(line.match(/has (\d+) positions/)[1]);
+    const startPosition = Number(line.match(/at time=0, it is at position (\d+)/)[1]);
+
+    return {
+      number,
+      positions,
+      startPosition
+    }
+  });
+
+const getPositionAtTime = (disc, time) => (time + disc.startPosition) % disc.positions;
+
+const getAllPositionsForTime = (discs, time) =>
+  discs.map(disc => getPositionAtTime(disc, time + disc.number));
+
+const findSuitableTime = (input) => {
+  const discs = parseInput(input);
+
+  // for part 2
+  discs.push({ number: discs.length + 1, positions: 11, startPosition: 0 });
+
+  let result;
+  let i = 0;
+
+  while (!result) {
+    const areAllAt0 = getAllPositionsForTime(discs, i).every(pos => pos === 0);
+
+    if (areAllAt0) result = i;
+
+    i++;
+  }
+
+  return result;
+}
+
+console.log(findSuitableTime(input));
+
